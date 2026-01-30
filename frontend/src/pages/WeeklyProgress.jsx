f661fe7a0aa42f9d81711ec3992dc364595fa01b
kepsTech <afolabionikepo0@gmail.com>
2026-01-30 11:37:35 +0200

    added some changes to workflow

--- frontend/src/pages/WeeklyProgress.jsx
+++ frontend/src/pages/WeeklyProgress.jsx
@@ -21,15 +21,7 @@ function WeeklyProgress() {
     setSelectedDay(day);
 
     // Map weekday to actual date (based on your seeded data week)
-    const dayToDateMap = {
-      Monday: "2026-02-23",
-      Tuesday: "2026-02-24",
-      Wednesday: "2026-02-25",
-      Thursday: "2026-02-26",
-      Friday: "2026-02-27",
-      Saturday: "2026-02-28",
-      Sunday: "2026-03-01"
-    };
+  
 
     const date = WEEK_DATES[day];
 
