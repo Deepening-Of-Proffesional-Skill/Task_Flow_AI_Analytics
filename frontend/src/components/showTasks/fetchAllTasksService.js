c06942e3c68728bd6ef1fd79cb8bb924f17c8ce8
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2026-02-03 13:17:40 +0200

    Change fetch all tasks service to use auth token

--- frontend/src/components/showTasks/fetchAllTasksService.js
+++ frontend/src/components/showTasks/fetchAllTasksService.js
@@ -1,15 +1,19 @@
+import axios from 'axios';
 
 export const fetchTasks = async () => {
     try {
-        const response =await fetch(`${import.meta.env.REACT_API_URL}/tasks`);
-        const data = await response.json();
-        if ( response.ok ) {
-            return data.tasks;
-        } else {
-            throw new Error(data.error || 'Failed to fetch tasks');
-        }
+        const token = localStorage.getItem('authToken');
+
+        const {data} = await axios.post(`${import.meta.env.VITE_API_URL || 'http://localhost:3000'}/fetchAllTasks`, 
+            {}, 
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            }
+        );
+        return data.tasks;
     } catch (error) {
         console.error('Error fetching tasks:', error);
         throw error;
     }
-}
+};
+
