27d9ad70fb9ac347d7da1e0c9c148cf812271ada
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2026-02-04 14:23:25 +0200

    Show no tasks component

--- frontend/src/components/showTasks/DailyTasks.jsx
+++ frontend/src/components/showTasks/DailyTasks.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Card, Container  } from 'react-bootstrap';
+import NoTasks from "./NoTasks";
 
 export default function DailyTasks({ sortedTasks }) {
     const groupedByDate = sortedTasks.reduce((groups, task) => {
@@ -18,28 +19,32 @@ export default function DailyTasks({ sortedTasks }) {
 
   return (
     <Container className="tasks-wrapper">
-        {Object.keys(groupedByDate).map((date) => (
-            <Card key={date} className="mb-4">
-                <Card.Body>
-                    <div className="due-date">
-                        {date}
-                    </div>
-                    <div className='d-flex flex-column gap-3'>
-                        {groupedByDate[date].map((task) => (
-                            <div key={task.id} className="task-item border rounded">
-                                <Card.Title className="mb-1">
-                                    {task.title}
-                                </Card.Title>
-                            
-                                <Card.Subtitle className="d-flex align-items-center gap-2">
-                                    <span className="title">{task.status}</span>
-                                </Card.Subtitle>
-                            </div>
-                        ))}
-                    </div>
-                </Card.Body>
-            </Card>
-        ))}
+        {sortedTasks.length > 0 ? (            
+            Object.keys(groupedByDate).map((date) => (
+                <Card key={date} className="mb-4">
+                    <Card.Body>
+                        <div className="due-date">
+                            {date}
+                        </div>
+                        <div className='d-flex flex-column gap-3'>
+                            {groupedByDate[date].map((task) => (
+                                <div key={task.id} className="task-item border rounded">
+                                    <Card.Title className="mb-1">
+                                        {task.title}
+                                    </Card.Title>
+                                
+                                    <Card.Subtitle className="d-flex align-items-center gap-2">
+                                        <span className="title">{task.status}</span>
+                                    </Card.Subtitle>
+                                </div>
+                            ))}
+                        </div>
+                    </Card.Body>
+                </Card>
+            ))
+        ) : (
+            <NoTasks />
+        )}
     </Container>
   )
 }
