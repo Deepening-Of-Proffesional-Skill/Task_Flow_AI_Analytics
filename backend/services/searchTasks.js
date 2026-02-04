e63294d7921be6170fb1152383391604e757e233
Gulegodage Upeksha Chiranthi <ugulegod23@students.oamk.fi>
2026-02-04 08:55:25 +0200

    Change query according to data type

--- backend/services/searchTasks.js
+++ backend/services/searchTasks.js
@@ -10,20 +10,23 @@ async function searchTasks(userId, title, deadline, category, priority, status) 
             query = query.ilike('title', `%${title}%`);
         }
         if (deadline) {
-            query = query.eq('deadline', deadline);
+            const start = `${deadline}T00:00:00`;
+            const end = `${deadline}T23:59:59.999`;
+
+            query = query.gte("deadline", start).lte("deadline", end);
         }
         if (category && category !== 'All') {
             query = query.eq('category', category);
         }
         if (priority && priority !== 'All') {
-            query = query.eq('priority', priority);
+            query = query.eq('priority', parseInt(priority));
         }
         if (status && status !== 'All') {
             query = query.eq('status', status);
         }
 
         const { data, error } = await query;
-        
+
         if (error) {
             const err = new Error('Error searching tasks');
             err.status = 500;
