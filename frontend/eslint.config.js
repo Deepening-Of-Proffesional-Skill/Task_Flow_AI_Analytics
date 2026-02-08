f2c7b2021c822b8d44e67106857cf6608427c45e
Archana Ojha <t3ojar01@students.oamk.fi>
2026-02-08 18:45:54 +0200

    solved lint error

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,20 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["**/*.test.{js,jsx}"],
+    languageOptions: { globals: globals.jest },
+  },
+  {
+    plugins: { react: pluginReact },
+    settings: { react: { version: "detect" } },
+  },
   pluginReact.configs.flat.recommended,
   {
     ignores: ["dist/", "build/", "node_modules/"],
